f2d93239ca346c27d6c85f87a0bf9cf773c4dbfd
Jacob Vorreuter <jake@heroku.com>
2011-02-22 17:36:13 -0800

    adding smove command

--- src/nsync_set.erl
+++ src/nsync_set.erl
@@ -2,7 +2,7 @@
 -export([command_hooks/0, handle/3]).
 
 command_hooks() ->
-    ["sadd", "srem"].
+    ["sadd", "srem", "smove"].
 
 handle("sadd", [Key, Member], Tid) ->
     Set = lookup(Tid, Key),
@@ -10,7 +10,14 @@ handle("sadd", [Key, Member], Tid) ->
 
 handle("srem", [Key, Member], Tid) ->
     Set = lookup(Tid, Key),
-    ets:insert(Tid, {Key, lists:delete(Member, Set)}).
+    ets:insert(Tid, {Key, lists:delete(Member, Set)});
+
+handle("smove", [Source, Dest, Member], Tid) ->
+    Set1 = lookup(Tid, Source),
+    Set2 = lookup(Tid, Dest),
+    NewSet1 = lists:delete(Member, Set1),
+    NewSet2 = lists:usort([Member|Set2]),
+    ets:insert(Tid, [{Source, NewSet1}, {Dest, NewSet2}]).
 
 lookup(Tid, Key) ->
     case ets:lookup(Tid, Key) of
